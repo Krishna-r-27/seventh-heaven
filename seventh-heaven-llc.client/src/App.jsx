49b12fd6f2cfb39ba4bd199326d4df7e41f82f2c
Krishna Ravalji <krishna.r@dotscoms.com>
2026-04-09 17:57:19 +0530

    testing

--- seventh-heaven-llc.client/src/App.jsx
+++ seventh-heaven-llc.client/src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
     return (
         <section className="py-8 sm:py-14 md:py-16 lg:py-100">
-            <div className="container mx-auto">
+            <div className="container mx-auto ">
                 <h1 className="text-red-500 text-3xl">Seventh Heaven LLC</h1>
             </div>
         </section>
