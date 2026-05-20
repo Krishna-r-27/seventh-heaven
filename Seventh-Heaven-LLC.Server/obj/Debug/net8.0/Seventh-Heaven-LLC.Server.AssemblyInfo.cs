145f4e9bdc1a96193e0f2ea2af3504c0a991bb6a
Krishna Ravalji <krishna.r@dotscoms.com>
2026-05-20 11:13:02 +0530

    update property detail/book now page

--- Seventh-Heaven-LLC.Server/obj/Debug/net8.0/Seventh-Heaven-LLC.Server.AssemblyInfo.cs
+++ Seventh-Heaven-LLC.Server/obj/Debug/net8.0/Seventh-Heaven-LLC.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Seventh-Heaven-LLC.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1494d2d91a07c47aaa28ef18ed931b91ccdc1fd8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b4a3ea6e93ef3e7f32c8c71b0db5c480c38af16")]
 [assembly: System.Reflection.AssemblyProductAttribute("Seventh-Heaven-LLC.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Seventh-Heaven-LLC.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
